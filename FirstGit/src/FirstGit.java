26b153642028a4f76d9b2da0a7a0c1f3dd560cc4
soy111 <31201406+soy111@users.noreply.github.com>
2017-10-23 17:44:49 +0900

    송지윤-_2

--- FirstGit/src/FirstGit.java
+++ FirstGit/src/FirstGit.java
@@ -2,7 +2,7 @@
 public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("Welcome Git!");
-		System.out.println("지윤_커밋얍얍얍");
+		System.out.println("지윤_커밋ㅇㅇ");
 	}
 
 }
